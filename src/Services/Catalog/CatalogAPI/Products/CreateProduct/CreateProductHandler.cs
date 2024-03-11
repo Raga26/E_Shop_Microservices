5b92445a1428ce92f6943503e8f5485801b1e548
Ragamaliga <mragamaliga@gmail.com>
2024-03-11 13:58:10 +0530

    create slice catalogAPI

---
+++ src/Services/Catalog/CatalogAPI/Products/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,34 @@
+﻿using BuildingBlocks.CQRS;
+using CatalogAPI.Models;
+using MediatR;
+
+namespace CatalogAPI.Products.CreateProduct
+{
+    public record CreateProductCommand(string Name , string Description , List<string> Category , string ImageFile , decimal Price )
+                                        :ICommand<CreateProductResult>;
+    public record CreateProductResult(Guid Id);
+    internal class CreateProductCommandHandler 
+                   : ICommandHandler<CreateProductCommand, CreateProductResult>
+    {
+        public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
+        {
+            //create product entity from command object
+            //save the database
+            //return result
+
+            var product = new Product
+            {
+                Name = command.Name,
+                Description = command.Description,
+                Category = command.Category,
+                ImageFile = command.ImageFile,
+                Price = command.Price
+            };
+
+            //save to database
+
+            //return result
+            return new CreateProductResult(Guid.NewGuid());
+        }
+    }
+}
