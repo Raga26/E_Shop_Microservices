e90454bd535d1f8be84b26e5210c900ab83cf467
Ragamaliga <mragamaliga@gmail.com>
2024-03-20 21:02:35 +0530

    get product slice

--- src/Services/Catalog/CatalogAPI/Products/CreateProduct/CreateProductEndpoint.cs
+++ src/Services/Catalog/CatalogAPI/Products/CreateProduct/CreateProductEndpoint.cs
@@ -1,7 +1,4 @@
-﻿using Carter;
-using Mapster;
-using MediatR;
-
+﻿
 namespace CatalogAPI.Products.CreateProduct
 {
     public record CreateProductRequest(string Name, string Description, List<string> Category, string ImageFile, decimal Price);
@@ -10,11 +7,16 @@ namespace CatalogAPI.Products.CreateProduct
     {
         public void AddRoutes(IEndpointRouteBuilder app)
         {
+                 //incoming request to product command object ,
+                 //send it using the mediatr and mediatr will trigger handler class
+                 //will get back the result and adapt the result to create product response
+
                  app.MapPost("/products",
                  async (CreateProductRequest request, ISender sender) =>
                  {
                      var command = request.Adapt<CreateProductCommand>();
 
+                     //sends to the controller
                      var result = await sender.Send(command);
 
                      var response = result.Adapt<CreateProductResponse>();
--- src/Services/Catalog/CatalogAPI/Products/CreateProduct/CreateProductHandler.cs
+++ src/Services/Catalog/CatalogAPI/Products/CreateProduct/CreateProductHandler.cs
@@ -1,13 +1,11 @@
-﻿using BuildingBlocks.CQRS;
-using CatalogAPI.Models;
-using MediatR;
-
+﻿
 namespace CatalogAPI.Products.CreateProduct
 {
     public record CreateProductCommand(string Name , string Description , List<string> Category , string ImageFile , decimal Price )
                                         :ICommand<CreateProductResult>;
     public record CreateProductResult(Guid Id);
-    internal class CreateProductCommandHandler 
+    //constructor injection is used here.
+    internal class CreateProductCommandHandler(IDocumentSession session) 
                    : ICommandHandler<CreateProductCommand, CreateProductResult>
     {
         public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
@@ -26,9 +24,11 @@ namespace CatalogAPI.Products.CreateProduct
             };
 
             //save to database
+            session.Store(product);
+            await session.SaveChangesAsync(cancellationToken);
 
             //return result
-            return new CreateProductResult(Guid.NewGuid());
+            return new CreateProductResult(product.Id);
         }
     }
 }
